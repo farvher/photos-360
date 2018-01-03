39370182d88ac95ed7b3906788b369b54896833f
farvher <fesanmiguel@misena.edu.co>
2018-01-03 11:03:24 -0500

    Creacion de photos 360

---
+++ src/main/java/co/com/ceet/metrocuadrado/photos360/Photos360Application.java
@@ -0,0 +1,12 @@
+package co.com.ceet.metrocuadrado.photos360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Photos360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Photos360Application.class, args);
+	}
+}

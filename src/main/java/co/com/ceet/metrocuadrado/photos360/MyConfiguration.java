af1d2c3580a01f90ad5f62cd61404e3bb75c87b9
farvher <fesanmiguel@misena.edu.co>
2018-01-03 14:11:33 -0500

    photos360

---
+++ src/main/java/co/com/ceet/metrocuadrado/photos360/MyConfiguration.java
@@ -0,0 +1,20 @@
+package co.com.ceet.metrocuadrado.photos360;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@org.springframework.context.annotation.Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}

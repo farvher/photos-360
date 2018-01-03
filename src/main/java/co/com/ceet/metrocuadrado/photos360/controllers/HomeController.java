0211d66208246c10de98d8b68cacff38f9c72143
farvher <fesanmiguel@misena.edu.co>
2018-01-03 13:37:11 -0500

    allow origin

--- src/main/java/co/com/ceet/metrocuadrado/photos360/controllers/HomeController.java
+++ src/main/java/co/com/ceet/metrocuadrado/photos360/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package co.com.ceet.metrocuadrado.photos360.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,7 @@ public class HomeController {
 	
 	public final static String INDEX = "index";
 	
+	@CrossOrigin()
 	@GetMapping("/")
 	public String index(){
 		

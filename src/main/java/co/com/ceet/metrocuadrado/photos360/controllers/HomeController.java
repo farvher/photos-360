bae1b44e4260300e97c5fc045c4eae6dff9a88a4
farvher <fesanmiguel@misena.edu.co>
2018-01-03 11:41:38 -0500

    [ADD] controlers y foto sample

---
+++ src/main/java/co/com/ceet/metrocuadrado/photos360/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package co.com.ceet.metrocuadrado.photos360.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	public final static String INDEX = "index";
+	
+	@GetMapping("/")
+	public String index(){
+		
+		return INDEX;
+	}
+
+}
